e77c8dc12e8e0ea372febe2c2d513aa5f0a852cb
Brandon Amos <bamos@cs.cmu.edu>
2015-07-03 15:04:21 -0400

    Code indentation.

--- validate.rb
+++ validate.rb
@@ -12,42 +12,48 @@ require 'w3c_validators'
 require 'colorize'
 require 'open-uri'
 
-IGNORED_FILES = [
-]
+puts "Validating jekyll output in '_site/'..."
+puts "\n"
+failed = 0
+passed = 0
+skipped = 0
 
-# Hack: w3c_validators doesn't provide a generic XML validator.
-# We provide a replacement based on Nokogiri with compatible interface.
 class XMLValidator < W3CValidators::Validator
-    def validate_file(file)
-        if file.respond_to? :read
-            src = file.read
-            file_path = file.path ||= nil
-        else
-            src = read_local_file(file)
-            file_path = file
-        end
-
-        begin
-            document = Nokogiri::XML(src)
-            @results = if document.xpath('*/@xsi:schemaLocation').empty?
-                W3CValidators::Results.new({:uri => nil, :validity => true})
-            else
-                schema_uri = document.xpath('*/@xsi:schemaLocation').to_s.split[1]
-                schema = Nokogiri::XML::Schema(open(schema_uri).read)
-                errors = schema.validate(document)
-                r = W3CValidators::Results.new({:uri => nil, :validity => errors.empty?})
-                errors.each { |msg| r.add_message(:error, msg.to_s) if msg.error? }
-            end
-        rescue
-            @results = W3CValidators::Results.new({:uri => nil, :validity => false})
-            @results.add_message(:error, 'Nokogiri threw errors on input.')
-        end
-        @results
+  def validate_file(file)
+    if file.respond_to? :read
+      src = file.read
+      file_path = file.path ||= nil
+    else
+      src = read_local_file(file)
+      file_path = file
     end
+
+    begin
+      document = Nokogiri::XML(src)
+      @results = if document.xpath('*/@xsi:schemaLocation').empty?
+                   W3CValidators::Results.new({:uri => nil, :validity => true})
+                 else
+                   schema_uri = document.xpath('*/@xsi:schemaLocation').to_s.split[1]
+                   schema = Nokogiri::XML::Schema(open(schema_uri).read)
+                   errors = schema.validate(document)
+                   r = W3CValidators::Results.new(
+                     {:uri => nil, :validity => errors.empty?}
+                   )
+                   errors.each { |msg|
+                     r.add_error({ :message => msg.to_s }) if msg.error?
+                   }
+                   r
+                 end
+    rescue
+      @results = W3CValidators::Results.new(
+        {:uri => nil, :validity => false}
+      )
+      @results.add_error({ :message => 'Nokogiri threw errors on input.' })
+    end
+    @results
+  end
 end
 
-# Hack: W3CValidators::NuValidator seems broken and the gem looks unmaintained.
-# We provide our own replacement based on the html5_validator gem, with compatible interface.
 class HtmlValidator < W3CValidators::Validator
     def validate_file(file)
         if file.respond_to? :read
@@ -60,50 +66,43 @@ class HtmlValidator < W3CValidators::Validator
 
         validator = Html5Validator::Validator.new
         validator.validate_text(src)
-        @results = W3CValidators::Results.new({:uri => nil, :validity => validator.valid?})
-        validator.errors.each { |err| @results.add_message(:error, err['message']) }
+        @results = W3CValidators::Results.new(
+          {:uri => nil, :validity => validator.valid?}
+        )
+        validator.errors.each { |err|
+          @results.add_error({ :message => err['message'] })
+        }
         @results
     end
 end
 
-puts "Validating jekyll output in '_site/'..."
-puts "\n"
-failed = 0
-passed = 0
-skipped = 0
-
-# Iterate over all the site files and validate them as appropriate.
 Dir.glob("_site/**/*") do |file|
-    # Skip ignored files and all directories
-    next if File.directory?(file)
-    next if IGNORED_FILES.include? file
+  next if File.directory?(file)
 
-    # Since all validators have compatible interfaces, we create the appropriate instance...
-    validator = case File.extname(file)
-    when '.html'
-        HtmlValidator.new
-    when '.xml'
-        if File.basename(file) == 'atom.xml'
-            W3CValidators::FeedValidator.new
-        else
-            XMLValidator.new
-        end
-    when '.css'
-        W3CValidators::CSSValidator.new
-    else
-        skipped += 1
-        puts file.colorize(:light_black)
-        next
-    end
+  validator = case File.extname(file)
+              when '.html'
+                HtmlValidator.new
+              when '.xml'
+                if File.basename(file) == 'atom.xml'
+                  W3CValidators::FeedValidator.new
+                else
+                  XMLValidator.new
+                end
+              when '.css'
+                W3CValidators::CSSValidator.new
+              else
+                skipped += 1
+                puts file.colorize(:light_black)
+                next
+              end
 
-    # ... and then run the validation.
-    if validator.validate_file(file).errors.empty?
-        puts file.colorize(:green)
-        passed += 1
-    else
-        puts file.colorize(:red)
-        failed += 1
-    end
+  if validator.validate_file(file).errors.empty?
+    puts file.colorize(:green)
+    passed += 1
+  else
+    puts file.colorize(:red)
+    failed += 1
+  end
 end
 
 puts "\n"
